52fb59c1f9019d5d75cd30c755a4d7c12988874b
letan-165 <2251160057@ut.edu.vn>
2025-10-07 15:31:59 +0700

    chore: config

---
+++ config/mongo.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"myapp/common/utils"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectMongo() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mongoURI := os.Getenv("MONGO_URI")
+	dbName := os.Getenv("DB_NAME")
+
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	ctx, cancel := utils.DefaultCtx()
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = client.Database(dbName)
+	log.Println("Connected to MongoDB Atlas")
+}
